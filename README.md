b6c08856d0ce1914235ff503be9941caaa89b4e6
StephaneDUPUY <38228196+StephaneDUPUY@users.noreply.github.com>
2020-03-18 15:33:24 +0100

    add prompts to enter use value and give answers + add screenshots

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Game - Find the number - 19/09/2019
+
+<img src = "screenshot1.jpg">
+
+<img src = "screenshot1.jpg">
+
+<img src = "screenshot3.jpg">
--- js/jeu.js
+++ js/jeu.js
@@ -10,12 +10,69 @@ let findTheNumber = {
         let randomNumber = findTheNumber.randomNumber(findTheNumber.valueMin, findTheNumber.valueMax);
         // display random number in console
         console.log(randomNumber);
+
+        // current quantity of try
+        let nbTry = 0;
+        // maximum of try
+        let nbMaxTry = 3;
+        // win status
+        let win = false;
+
+        // check quantity of try different of maximum of try and if win value is false
+        while(nbTry != nbMaxTry && !win){
+
+            // prompt give popup to enter a value
+            let userNumber = prompt('Veuillez saisir un nombre pour jouer');
+
+            // parseInt force value given to be converted in integer
+            userNumber = parseInt(userNumber);
+
+            // check if number converted is a number
+            if(!isNaN(userNumber)){
+
+                // if number given is good one then success
+                if(randomNumber === userNumber){
+
+                    alert('Victoire !');
+
+                    // end of loop
+                    win = true;
+
+                } else { // else lose
+
+                    if(userNumber > randomNumber){
+
+                        alert("Trop grand !");
+                    } else {
+                        alert("Trop petit !");
+                    }
+
+                }
+
+
+            } else {
+
+               alert('Saisie invalide: chiffre attendu');
+
+               gamePlusPetitPlusGrand.play();
+            }
+
+            nbTry += 1;
+
+        }
+
+        if(!win){
+
+            alert('Perdu !');
+        }
     },
+
     // give the integer of a number
     integer: function(number){
 
         return Math.floor(number);
     },
+
     // give a random and integer number between minimum and maximum values
     randomNumber: function(min, max){
         // give a random number between minimum and maximum values
